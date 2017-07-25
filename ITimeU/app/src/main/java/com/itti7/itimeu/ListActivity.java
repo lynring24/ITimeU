4dc7629bf6c81685cd93baba2243f84a686630b1
Hyemin Kim <1117hyemin@gmail.com>
2017-07-26 01:59:31 +0900

    오늘의 월/일을 TextView에 출력하는 함수작성

--- ITimeU/app/src/main/java/com/itti7/itimeu/ListActivity.java
+++ ITimeU/app/src/main/java/com/itti7/itimeu/ListActivity.java
@@ -2,6 +2,10 @@ package com.itti7.itimeu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by hyemin on 17. 7. 26.
@@ -12,5 +16,17 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        TextView textView = (TextView) findViewById(R.id.date_txt_view);
+        textView.setText(getDate());
+    }
+
+    /**
+     * It is a function of today's date.
+     * @return  Return the current month and day.
+     */
+    public String getDate(){
+        String today = new SimpleDateFormat("MM.dd").format(new Date());
+        return today;
     }
 }
