88a19e9bcf37f4a4aab73b8b1a23dc571fc94b61
Hyemin Kim <1117hyemin@gmail.com>
2017-07-26 01:01:32 +0900

    List를 위한 java 파일과 xml 파일 생성

---
+++ ITimeU/app/src/main/java/com/itti7/itimeu/ListActivity.java
@@ -0,0 +1,16 @@
+package com.itti7.itimeu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hyemin on 17. 7. 26.
+ */
+
+public class ListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
