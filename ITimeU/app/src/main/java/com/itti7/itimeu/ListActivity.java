e3e4787c2001f44e06062628775a48b4e2d2049a
Hyemin Kim <1117hyemin@gmail.com>
2017-07-26 20:03:11 +0900

    아이템 Add dialog xml 작성
    OK 클릭 시 토스트 메시지만 나오는 상태.

--- ITimeU/app/src/main/java/com/itti7/itimeu/ListActivity.java
+++ ITimeU/app/src/main/java/com/itti7/itimeu/ListActivity.java
@@ -1,8 +1,14 @@
 package com.itti7.itimeu;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -19,6 +25,14 @@ public class ListActivity extends AppCompatActivity {
 
         TextView textView = (TextView) findViewById(R.id.date_txt_view);
         textView.setText(getDate());
+
+        final FloatingActionButton addFab = (FloatingActionButton) this.findViewById(R.id.add_fab_btn);
+        addFab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                showAddDialog();
+            }
+        });
     }
 
     /**
@@ -28,5 +42,36 @@ public class ListActivity extends AppCompatActivity {
     public String getDate(){
         String today = new SimpleDateFormat("MM.dd").format(new Date());
         return today;
+    }
+
+
+    private void showAddDialog(){
+        LayoutInflater dialog = LayoutInflater.from(this);
+        final View dialogLayout = dialog.inflate(R.layout.add_dialog, null);
+        final Dialog addDialog = new Dialog(this);
+
+        addDialog.setContentView(dialogLayout);
+        addDialog.show();
+
+        Button mOkButton = dialogLayout.findViewById(R.id.add_ok_btn);
+        Button mCancelButton = dialogLayout.findViewById(R.id.add_cancel_btn);
+
+        mOkButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mCancelButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                addDialog.cancel();
+            }
+        });
     }
 }
