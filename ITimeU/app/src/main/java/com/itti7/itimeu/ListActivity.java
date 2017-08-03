1565e4abcbfac738973d7a5f9a9d35d7ceb98d2f
Hyemin Kim <1117hyemin@gmail.com>
2017-08-03 20:32:28 +0900

    Context menu 추가, delete 기능 추가

--- ITimeU/app/src/main/java/com/itti7/itimeu/ListActivity.java
+++ ITimeU/app/src/main/java/com/itti7/itimeu/ListActivity.java
@@ -1,37 +1,25 @@
 package com.itti7.itimeu;
 
-import android.app.Dialog;
 import android.app.LoaderManager;
 import android.content.ContentUris;
-import android.content.ContentValues;
 import android.content.CursorLoader;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import com.itti7.itimeu.data.ItemContract.ItemEntry;
-import com.itti7.itimeu.data.ItemDbHelper;
 
 /**
  * Displays list of pets that were entered and stored in the app.
@@ -44,6 +32,8 @@ public class ListActivity extends AppCompatActivity implements
      * Identifier for the item data loader
      */
     private static final int ITEM_LOADER = 0;
+
+    ListView itemListView;
 
     /**
      * Adapter for the ListView
@@ -67,7 +57,7 @@ public class ListActivity extends AppCompatActivity implements
         });
 
         // Find the ListView which will be populated with the item data
-        ListView itemListView = (ListView) findViewById(R.id.item_list_view);
+        itemListView = (ListView) findViewById(R.id.item_list_view);
 
         // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
         View emptyView = findViewById(R.id.empty_relative_view);
@@ -78,7 +68,7 @@ public class ListActivity extends AppCompatActivity implements
         mCursorAdapter = new ItemCursorAdapter(this, null);
         itemListView.setAdapter(mCursorAdapter);
 
-        // Setup the item long click listener
+        /*// Setup the item long click listener
         itemListView.setOnItemLongClickListener(new AdapterView
                 .OnItemLongClickListener() {
             @Override
@@ -93,10 +83,46 @@ public class ListActivity extends AppCompatActivity implements
 
                 return true;
             }
-        });
+        });*/
+
+        registerForContextMenu(itemListView);
 
         // Kick off the loader
         getLoaderManager().initLoader(ITEM_LOADER, null, this);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info =
+                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        // click item's index
+        int id = (int) info.id;
+
+        Intent intent = new Intent(ListActivity.this, EditorActivity.class);
+        Uri currentItemUri = ContentUris.withAppendedId(ItemEntry.CONTENT_URI, id);
+        intent.setData(currentItemUri);
+
+        switch (item.getItemId()) {
+            case R.id.action_modify:
+                startActivity(intent);
+                break;
+            case R.id.action_delete:
+                showDeleteConfirmationDialog(id);
+                break;
+            default:
+                return false;
+        }
+
+        return true;
     }
 
     @Override
@@ -116,6 +142,58 @@ public class ListActivity extends AppCompatActivity implements
                 null,                   // No selection clause
                 null,                   // No selection arguments
                 null);                  // Default sort order
+    }
+
+    /**
+     * Prompt the user to confirm that they want to delete this pet.
+     */
+    private void showDeleteConfirmationDialog(final int index) {
+        // Create an AlertDialog.Builder and set the message, and click listeners
+        // for the postivie and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Do you want to delete this item?");
+        builder.setPositiveButton("delete", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Delete" button, so delete the pet.
+                deleteItem(index);
+            }
+        });
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Cancel" button, so dismiss the dialog
+                // and continue editing the pet.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void deleteItem(int index) {
+        Uri currentItemUri = ContentUris.withAppendedId(ItemEntry.CONTENT_URI, index);
+
+        // Only perform the delete if this is an existing pet.
+        if (currentItemUri != null) {
+            // Call the ContentResolver to delete the pet at the given content URI.
+            // Pass in null for the selection and selection args because the mCurrentPetUri
+            // content URI already identifies the pet that we want.
+            int rowsDeleted = getContentResolver().delete(currentItemUri, null, null);
+
+            // Show a toast message depending on whether or not the delete was successful.
+            if (rowsDeleted == 0) {
+                // If no rows were deleted, then there was an error with the delete.
+                Toast.makeText(this, "fail to delete",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the delete was successful and we can display a toast.
+                Toast.makeText(this, "success to delete",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
