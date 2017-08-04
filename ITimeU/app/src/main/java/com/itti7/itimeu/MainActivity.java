4962a17f0528d7da4f6ef09c82aaf0d75963c500
lyn <lync2846@gmail.com>
2017-08-04 14:11:49 +0900

    add nav bar to main layout

--- ITimeU/app/src/main/java/com/itti7/itimeu/MainActivity.java
+++ ITimeU/app/src/main/java/com/itti7/itimeu/MainActivity.java
@@ -1,7 +1,10 @@
 package com.itti7.itimeu;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,27 +19,59 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.timer_container, new TimerFragment()).commit();
 /*Toolbar init*/
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.menu_toolbar);
-        setSupportActionBar(myToolbar);
+        BottomNavigationView bottomNavigation =
+                (BottomNavigationView)  findViewById(R.id.menu_toolbar);
+        bottomNavigation.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        int id = item.getItemId(); //pressed item ID
+                        if (id == R.id.category_about) {
+                /*Intent passTo  = new Intent(getApplicationContext(),SettingActivity);
+                * startActivity(passTo);*/
+//            Toast.makeText(this, "setting", Toast.LENGTH_SHORT).show(); // test code
+                            return true;
+                        }
+                        if (id == R.id.category_statistics) {
+            /*Intent passTo  = new Intent(getApplicationContext(),StatisticsActivity);
+                * startActivity(passTo);*/
+                            return true;
+                        }
+                        if (id == R.id.category_setting) {
+            /*Intent passTo  = new Intent(getApplicationContext(),StatisticsActivity);
+                * startActivity(passTo);*/
+                            return true;
+                        }
+                        return true;
+                    }
+                });
+        if (savedInstanceState == null) {
+            bottomNavigation.setSelectedItemId(R.id.category_statistics);
+        }
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
-    public boolean onOptionsItemSelected(MenuItem item){
+
+}
+/*    public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId(); //pressed item ID
-        if (id == R.id.action_setting) {
-                /*Intent passTo  = new Intent(getApplicationContext(),SettingActivity);
-                * startActivity(passTo);*/
+        if (id == R.id.category_about) {
+                *//*Intent passTo  = new Intent(getApplicationContext(),SettingActivity);
+                * startActivity(passTo);*//*
 //            Toast.makeText(this, "setting", Toast.LENGTH_SHORT).show(); // test code
             return true;
         }
-        if (id == R.id.action_statistics) {
-            /*Intent passTo  = new Intent(getApplicationContext(),StatisticsActivity);
-                * startActivity(passTo);*/
+        if (id == R.id.category_statistics) {
+            *//*Intent passTo  = new Intent(getApplicationContext(),StatisticsActivity);
+                * startActivity(passTo);*//*
+            return true;
+        }
+        if (id == R.id.category_setting) {
+            *//*Intent passTo  = new Intent(getApplicationContext(),StatisticsActivity);
+                * startActivity(passTo);*//*
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-}
+    }*/
