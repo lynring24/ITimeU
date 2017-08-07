a50945cd5b5f41aaec5e8cfe743fbc235cefeb4a
lyn <lync2846@gmail.com>
2017-08-07 16:08:09 +0900

    add chart to statistic Activity

--- ITimeU/app/src/main/java/com/itti7/itimeu/StatisticsActivity.java
+++ ITimeU/app/src/main/java/com/itti7/itimeu/StatisticsActivity.java
@@ -2,22 +2,135 @@ package com.itti7.itimeu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class StatisticsActivity extends AppCompatActivity {
-    private Button mTestor;
+    private RelativeLayout mLineLayout;
+    private LineChart mChart;
+    private float[] yData = {5, 10, 7, 8, 13};
+    private String[] xData = { "Done", "Still", "LG", "Apple111", "Samsung"};
+    private Spinner statSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
-        mTestor=(Button)findViewById(R.id.button_testor);
-        mTestor.setOnClickListener(new View.OnClickListener() {
+        statSpinner = (Spinner)findViewById(R.id.stat_spinner);
+
+        ArrayAdapter statAdapter = ArrayAdapter.createFromResource(this, R.array.stat_type, android.R.layout.simple_spinner_item);
+        statAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        statSpinner.setAdapter(statAdapter);
+
+
+
+        mLineLayout = (RelativeLayout)findViewById(R.id.stat_chart);
+        mChart = new LineChart(this);
+
+        // add pie chart to main layout
+        mLineLayout.addView(mChart);
+        //mPieLayout.setBackgroundColor(Color.LTGRAY);
+
+        // configure pie chart
+        mChart.setDescription("Session works");
+
+
+        // set a chart value selected listener
+        mChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"got in",Toast.LENGTH_LONG).show();
+            public void onValueSelected(Entry entry, int i) {
+                if (entry == null)
+                    return;
+
+                Toast.makeText(getBaseContext(), "Data : " + xData[entry.getXIndex()] + " , value : " + entry.getVal(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected() {
+
             }
         });
+
+        // add data
+        addData();
+
+        // customize legends
+        Legend l = mChart.getLegend();
+        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
+        l.setXEntrySpace(7);
+        l.setYEntrySpace(5);
+    }
+
+    private void addData() {
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+
+        for (int i = 0; i < yData.length; i++)
+            yVals1.add(new Entry(yData[i],i));
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        for (int i = 0; i < xData.length; i++)
+            xVals.add(xData[i]);
+
+        // create pie data set
+        LineDataSet dataSet = new LineDataSet(yVals1, "Market Share");
+
+        // add mana colors
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+
+        // instantiate pie data object now
+        LineData data = new LineData(xVals, dataSet);
+
+        //mChart.setValueFormatter();
+        mChart.setValueTextSize(14f);
+        mChart.setValueTextColor(Color.BLACK);
+
+        mChart.setData(data);
+
+        // undo all highlights
+        mChart.highlightValues(null);
+
+        // update pie chart
+        mChart.invalidate();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
+
