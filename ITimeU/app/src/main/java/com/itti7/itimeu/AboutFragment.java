292e0654df017283eb1638d2ea5aa10ee33d6610
Hyemin Kim <1117hyemin@gmail.com>
2017-08-21 12:29:42 +0900

    Open licenses 텍스트뷰 클릭 시 라이센스 정보가 들어있는 액티비티 시작

--- ITimeU/app/src/main/java/com/itti7/itimeu/AboutFragment.java
+++ ITimeU/app/src/main/java/com/itti7/itimeu/AboutFragment.java
@@ -1,13 +1,15 @@
 package com.itti7.itimeu;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,14 +20,19 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class AboutFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // This application's Version
+    final static String APPLICATION_VERSION = "0.0.0";
+
+    // This Fragment's inflater, activity, context
+    View mAboutView;
+    Activity mAboutActivity;
+    Context mAboutContext;
+
+    // TextView for setting version text
+    TextView mVersionTextView;
+    // TextView to show Activity for open licenses
+    TextView mLicensesTextView;
 
     private OnFragmentInteractionListener mListener;
 
@@ -37,16 +44,12 @@ public class AboutFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment AboutFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AboutFragment newInstance(String param1, String param2) {
+    public static AboutFragment newInstance() {
         AboutFragment fragment = new AboutFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,16 +58,33 @@ public class AboutFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        mAboutView = inflater.inflate(R.layout.fragment_about, container, false);
+        mAboutActivity = getActivity();
+        mAboutContext = mAboutView.getContext();
+
+        // Get Version TextView for setting text
+        mVersionTextView = mAboutView.findViewById(R.id.version_txt_view);
+        mVersionTextView.setText(APPLICATION_VERSION);
+
+        // When click this text view, than start an activity which show licenses's info
+        mLicensesTextView = mAboutView.findViewById(R.id.licenses_txt_view);
+        mLicensesTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mAboutContext, LicensesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return mAboutView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
