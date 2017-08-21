74ed1b03d7e99cd56802a01f5cdd98bfaf9371aa
Hyemin Kim <1117hyemin@gmail.com>
2017-08-21 14:32:19 +0900

    라이센스 액티비티 백버튼 추가
    이미지 버튼 UI, 뒤로가기 버튼 누를 시 finish() 호출하여 액티비티 종료

--- ITimeU/app/src/main/java/com/itti7/itimeu/LicensesActivity.java
+++ ITimeU/app/src/main/java/com/itti7/itimeu/LicensesActivity.java
@@ -2,12 +2,31 @@ package com.itti7.itimeu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class LicensesActivity extends AppCompatActivity {
+
+    ImageButton mBackImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_licenses);
+
+        mBackImageButton = (ImageButton) findViewById(R.id.back_img_btn);
+        mBackImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
