f5a7cb17c108c02890ce6b2b302f6443d1d80214
Hyemin Kim <1117hyemin@gmail.com>
2017-08-22 14:10:13 +0900

    버전 3.0.0 으로 변경하면서 생기는 에러 코드 삭제

--- ITimeU/app/src/main/java/com/itti7/itimeu/StatisticsFragment.java
+++ ITimeU/app/src/main/java/com/itti7/itimeu/StatisticsFragment.java
@@ -2,7 +2,6 @@ package com.itti7.itimeu;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,81 +10,59 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
 public class StatisticsFragment extends Fragment {
-
+    // Identifier for the item data loader
     private View mStatisticsView;
     private Activity mStatisticsActivity;
     private Context mStatisticsContext;
 
-    private RelativeLayout mLineLayout;
+    // Statistics view components
+    private Spinner mStatSpinner;
     private LineChart mChart;
-    private float[] yData = {5, 10, 7, 8, 13};
-    private String[] xData = { "Done", "Still", "LG", "Apple111", "Samsung"};
-    private Spinner statSpinner;
+    private TextView mStatResultText;
 
+    // xData: date, yData: unit
+    private int[] yData = {0};
+    private String[] xData = {""};
+
+
+    /*ToDo:
+        0. Spinner 아이템에 따른 토스트창 띄우기
+        1. week, month, custom 에 해당하는 unit, total unit 정보 얻어오기
+        2. 리스트 엔트리에 저장하기(x축은 날짜, y축은 포모도로 단위)
+        3. 데이터 셋에 저장하기
+        4. 차트에 데이터 뿌리기
+        5. UI 커스터마이징 하기
+     */
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        // Set identifier
         mStatisticsView = inflater.inflate(R.layout.fragment_statistics, container, false);
         mStatisticsActivity = getActivity();
         mStatisticsContext = mStatisticsView.getContext();
 
-        statSpinner = mStatisticsView.findViewById(R.id.stat_spinner);
+        // Get id from fragment_statistics
+        mStatSpinner = mStatisticsView.findViewById(R.id.stat_spinner);
+        mChart = mStatisticsView.findViewById(R.id.chart);
+        mStatResultText = mStatisticsView.findViewById(R.id.stat_result);
 
-        ArrayAdapter statAdapter = ArrayAdapter.createFromResource(mStatisticsContext, R.array.stat_type, android.R.layout.simple_spinner_item);
-        statAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        statSpinner.setAdapter(statAdapter);
-
-
-
-        mLineLayout = mStatisticsView.findViewById(R.id.stat_chart);
-        mChart = new LineChart(mStatisticsContext);
-
-        // add pie chart to main layout
-        mLineLayout.addView(mChart);
-        //mPieLayout.setBackgroundColor(Color.LTGRAY);
-
-        // configure pie chart
-        mChart.setDescription("Session works");
-
-
-        // set a chart value selected listener
-        mChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
-            @Override
-            public void onValueSelected(Entry entry, int i) {
-                if (entry == null)
-                    return;
-
-                Toast.makeText(mStatisticsActivity.getBaseContext(), "Data : " + xData[entry.getXIndex()] + " , value : " + entry.getVal(), Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected() {
-
-            }
-        });
-
-        // add data
+        // Add data
         addData();
 
-        // customize legends
+        // Customize legends
         Legend l = mChart.getLegend();
-        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
         l.setXEntrySpace(7);
         l.setYEntrySpace(5);
 
@@ -98,21 +75,26 @@ public class StatisticsFragment extends Fragment {
         setHasOptionsMenu(true);
     }
 
+    /**
+     * Set data to chart view (?)
+     */
     private void addData() {
         ArrayList<Entry> yVals1 = new ArrayList<Entry>();
 
-        for (int i = 0; i < yData.length; i++)
-            yVals1.add(new Entry(yData[i],i));
+        for (int i = 0; i < yData.length; i++) {
+            yVals1.add(new Entry(yData[i], i));
+        }
 
         ArrayList<String> xVals = new ArrayList<String>();
 
-        for (int i = 0; i < xData.length; i++)
+        for (int i = 0; i < xData.length; i++) {
             xVals.add(xData[i]);
+        }
 
-        // create pie data set
+        // Create pie data set
         LineDataSet dataSet = new LineDataSet(yVals1, "Market Share");
 
-        // add mana colors
+        // Add mana colors
         ArrayList<Integer> colors = new ArrayList<Integer>();
 
         for (int c : ColorTemplate.VORDIPLOM_COLORS)
@@ -132,22 +114,11 @@ public class StatisticsFragment extends Fragment {
 
         colors.add(ColorTemplate.getHoloBlue());
 
-        // instantiate pie data object now
-        LineData data = new LineData(xVals, dataSet);
-
-        //mChart.setValueFormatter();
-        mChart.setValueTextSize(14f);
-        mChart.setValueTextColor(Color.BLACK);
-
-        mChart.setData(data);
-
-        // undo all highlights
+        // Undo all highlights
         mChart.highlightValues(null);
 
-        // update pie chart
+        // Update pie chart
         mChart.invalidate();
-
-
     }
 
     @Override
