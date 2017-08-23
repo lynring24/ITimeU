89be0e0fc80f948679441e2a34e00f2ba5d3d8ad
Hyemin Kim <1117hyemin@gmail.com>
2017-08-23 16:10:18 +0900

    불필요한 import 삭제

--- ITimeU/app/src/main/java/com/itti7/itimeu/StatisticsFragment.java
+++ ITimeU/app/src/main/java/com/itti7/itimeu/StatisticsFragment.java
@@ -2,14 +2,12 @@ package com.itti7.itimeu;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
