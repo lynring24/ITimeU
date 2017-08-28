fb6d910e77078cc08c576dc1ec25728a8ca9d7d4
Hyemin Kim <1117hyemin@gmail.com>
2017-08-28 09:39:34 +0900

    혼란 방지를 위해 이름 리팩토링

--- ITimeU/app/src/main/java/com/itti7/itimeu/ItemCursorAdapter.java
+++ ITimeU/app/src/main/java/com/itti7/itimeu/ItemCursorAdapter.java
@@ -40,7 +40,7 @@ class ItemCursorAdapter extends CursorAdapter {
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+        return LayoutInflater.from(context).inflate(R.layout.item, parent, false);
     }
 
     /**
