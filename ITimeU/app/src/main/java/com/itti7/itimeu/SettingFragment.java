15296c22c386a6d8e54206dcbc80141eb990842f
jiun\jiunoh <juneoh227@gmail.com>
2017-08-31 21:40:48 +0900

    체크박스 저장

--- ITimeU/app/src/main/java/com/itti7/itimeu/SettingFragment.java
+++ ITimeU/app/src/main/java/com/itti7/itimeu/SettingFragment.java
@@ -5,11 +5,14 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.SeekBar;
 
@@ -24,13 +27,16 @@ public class SettingFragment extends Fragment {
 
     private SeekBar mworksb, mbreaksb, mlongBreaksb, msessionNumsb; //시크바
     private static EditText mworket, mbreaket, mlongBreaket, msessionNumet; //에디트텍스트 뷰
-    private static CheckBox msoundOn, mvibrateOn, monScreen;
+    private static CheckBox msoundOncb, mvibrateOncb, monScreencb; //체크박스
 
     public static final String PREFNAME = "SETTING_PREFERENCE";
     public static final String WORKTIME = "worktime";
     public static final String BREAKTIME = "breaktime";
     public static final String LONGBREAKTIME = "longbreaktime";
     public static final String SESSION = "session";
+    public  static final String SCREENON = "screen"; //boolean, 참이면 켜짐
+    public  static final String SOUNDON = "sound";
+    public  static final String VIBRATEON = "vibrate";
     //설정 저장에 필요한 상수(이름)
 
     public static EditText getMworket() {
@@ -69,21 +75,29 @@ public class SettingFragment extends Fragment {
         msessionNumet = mSettingView.findViewById(R.id.session_number);
         ///////각 에디트텍스트
 
-        monScreen = mSettingView.findViewById(R.id.screen_check);
-        msoundOn = mSettingView.findViewById(R.id.sound_check);
-        mvibrateOn = mSettingView.findViewById(R.id.vibrate_check);
+        //각 체크박스
+        monScreencb = mSettingView.findViewById(R.id.screen_check);
+        msoundOncb = mSettingView.findViewById(R.id.sound_check);
+        mvibrateOncb = mSettingView.findViewById(R.id.vibrate_check);
 
-        //저장해둔 설정 불러오기
         SharedPreferences pref = getActivity().getSharedPreferences(PREFNAME, Context.MODE_PRIVATE);
+
+        //저장해둔 숫자 설정 불러오기
         mworket.setText(String.valueOf(pref.getInt(WORKTIME, 25)));
         mbreaket.setText(String.valueOf(pref.getInt(BREAKTIME, 5)));
         mlongBreaket.setText(String.valueOf(pref.getInt(LONGBREAKTIME, 20)));
         msessionNumet.setText(String.valueOf(pref.getInt(SESSION, 4)));
 
+        //숫자-시크바 연동
         mworksb.setProgress(pref.getInt(WORKTIME, 25));
         mbreaksb.setProgress(pref.getInt(BREAKTIME, 5));
         mlongBreaksb.setProgress(pref.getInt(LONGBREAKTIME, 20));
         msessionNumsb.setProgress(pref.getInt(SESSION, 4));
+
+        //체크박스 설정 불러오기
+        msoundOncb.setChecked(pref.getBoolean(SOUNDON, true));
+        mvibrateOncb.setChecked(pref.getBoolean(VIBRATEON, false));
+        monScreencb.setChecked(pref.getBoolean(SCREENON, false));
 
         mworksb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -129,6 +143,24 @@ public class SettingFragment extends Fragment {
             }
         }); //세션 수 시크바 리스너
 
+        monScreencb.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                saveCheckBox(buttonView, isChecked);
+            }
+        });
+
+        msoundOncb.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                saveCheckBox(buttonView, isChecked);
+            }
+        });
+
+        mvibrateOncb.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                saveCheckBox(buttonView, isChecked);
+            }
+        });
+
         return mSettingView;
     }
 
@@ -138,7 +170,7 @@ public class SettingFragment extends Fragment {
 
     }
 
-    public void printSelected(SeekBar bar, int value) { // 이용자가 바를 누르고 있을 때의 숫자 출력
+    private void printSelected(SeekBar bar, int value) { // 이용자가 바를 누르고 있을 때의 숫자 출력
         if (bar.equals(mworksb)) {
             mworket.setText(String.valueOf(value));
         }
@@ -153,7 +185,7 @@ public class SettingFragment extends Fragment {
         }
     }
 
-    public void doAfterTrack(SeekBar bar) { // 이용자가 손을 뗐을 때의 숫자 출력
+    private void doAfterTrack(SeekBar bar) { // 이용자가 손을 뗐을 때의 숫자 출력
         SharedPreferences pref = getActivity().getSharedPreferences(PREFNAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
 
@@ -172,6 +204,21 @@ public class SettingFragment extends Fragment {
         else if (bar.equals(msessionNumsb)) {
             msessionNumet.setText(msessionNumet.getText());
             editor.putInt(SESSION, Integer.valueOf(msessionNumet.getText().toString()));
+        }
+        editor.commit();
+    }
+
+    private void saveCheckBox(CompoundButton cbox, boolean isChecked) {
+        SharedPreferences pref = getActivity().getSharedPreferences(PREFNAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        if (cbox.equals(monScreencb)) {
+            editor.putBoolean(SCREENON, isChecked);
+        }
+        else if (cbox.equals(msoundOncb)) {
+            editor.putBoolean(SOUNDON, isChecked);
+        }
+        else if (cbox.equals(mvibrateOncb)) {
+            editor.putBoolean(VIBRATEON, isChecked);
         }
         editor.commit();
     }
