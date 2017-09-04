050fa261bf6df1145c15ac731061bf0005f4d904
Hyemin Kim <1117hyemin@gmail.com>
2017-09-04 15:33:44 +0900

    메인에서 back 버튼 클릭 시 토스트 알람 띄운 뒤 finish

--- ITimeU/app/src/main/java/com/itti7/itimeu/MainActivity.java
+++ ITimeU/app/src/main/java/com/itti7/itimeu/MainActivity.java
@@ -1,11 +1,13 @@
 package com.itti7.itimeu;
 
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -73,6 +75,27 @@ public class MainActivity extends AppCompatActivity {
                 tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
         viewPager.setOffscreenPageLimit(4);
+    }
+
+    private Boolean exit = false;
+    @Override
+    public void onBackPressed() {
+        if (exit) {
+            super.onBackPressed();
+            finish(); // finish activity
+        } else {
+            Toast.makeText(this, "@string/main_press_back",
+                    Toast.LENGTH_SHORT).show();
+            exit = true;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    exit = false;
+                }
+            }, 3 * 1000);
+
+        }
+
     }
 
     /**
